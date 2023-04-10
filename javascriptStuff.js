bb30cfc80bd578e9a2b6c2fbbc05bcecf6e7b6d8
Brian Voter <bvoter97@gmail.com>
2023-04-10 19:31:59 -0400

    finish part one

--- javascriptStuff.js
+++ javascriptStuff.js
@@ -1,10 +1,23 @@
- "use strict";
+"use strict";
 
- $("article img").addClass("image-center");
+$("article img").addClass("image-center");
 
- $("article p").last().remove();
+$("article p").last().remove();
 
- $("#title").css("font-size", `${Math.floor(Math.random()*100) + 1}px` );
+$("#title").css("font-size", `${Math.floor(Math.random() * 100) + 1}px`);
 
+$("ol").append($("<li>", {text:"This is the million dollar answer."}));
 
+$("aside").empty().append($("<p>I'm so sorry about that list.</p>"));
 
+$("input").on("change", evt => {
+  const red = $("input").eq(0).val();
+  const blue = $("input").eq(1).val();
+  const green = $("input").eq(2).val();
+
+  $("body").css("background-color", `rgb(${red}, ${green}, ${blue})`);
+});
+
+$("img").on("click", evt => {
+  $(evt.target).remove();
+});
